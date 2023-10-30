d04a1bf71029eeae6302addb5ac06c37927bc7e3
Sam Bennett <sambennettdev@gmail.com>
2023-10-30 14:19:39 -0600

    moved functions to own files

--- ChessPlayground.py
+++ ChessPlayground.py
@@ -1,4 +1,29 @@
 import chess
-import PieceTables as tables
+from EvaluatePos import *
+from ChessBot import *
+
 board = chess.Board()
 print(board.fen)
+
+
+def check_game_end(board):
+    if board.is_checkmate():
+        if board.turn:
+            return -9999 #black wins
+        else:
+            return 9999 #white wins
+
+    if board.is_stalemate():
+        return 0
+    if board.is_insufficient_material():
+        return 0
+
+indv_scores = calc_indv_score(board)
+piece_count, mat_score = calc_total_mat(board)
+advantage = calc_advantage(board, indv_scores, mat_score)
+
+print(indv_scores)
+print(piece_count, mat_score)
+print(advantage)
+
+print(move_in_book(board))
