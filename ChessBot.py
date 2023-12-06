1376ea88ee7118e4a2edea6a923860a678ed1688
Sam Bennett <sambennettdev@gmail.com>
2023-12-05 22:55:05 -0700

    Luna Chess NN integration working to make move

--- ChessBot.py
+++ ChessBot.py
@@ -1,5 +1,6 @@
 import torch
 import chess
+import numpy as np
 from ChessBotModel import ChessBotModel
 
 class ChessBot:
@@ -12,8 +13,6 @@ class ChessBot:
         except:
             self.save_model()
 
-        
-    
     def save_model(self):
         path = 'models/' + self.trainer_name + '.pth'
         torch.save(self.model.state_dict(), path)
@@ -31,55 +30,136 @@ class ChessBot:
 
         return best_move
 
+    def to_move(self, action):
+        pi_np, v_np = action
+
+        # Sample a move from the probability distribution
+        sampled_index = np.random.choice(len(pi_np), p=pi_np)
+        to_sq = sampled_index % 64
+        from_sq = int(sampled_index / 64)
+
+        return chess.Move(from_sq, to_sq)
+
+
     def select_best_move(self, board, legal_moves):
-        # Convert the board and legal moves into a format suitable for your model
+    # Convert the board and legal moves into a format suitable for your model
         board_tensor = self.board_to_tensor(board)
-        legal_moves_tensor = self.moves_to_tensor(board)
+        valid_tensor = self.moves_to_tensor(board)
+
+        board_tensor = board_tensor.view(1, 8, 8, 6)
 
         # Use the neural network to predict move values
-        move_values = self.model(board_tensor, legal_moves_tensor)
+        # predict without changing weights
+        self.model.eval()
+        with torch.no_grad():
+            pi, v = self.model((board_tensor, valid_tensor))
 
-        # Select the move with the highest predicted value
-        best_move_index = torch.argmax(move_values).item()
-        best_move = legal_moves[best_move_index]
+        # Extract the action values from the output
+        pi_np = torch.exp(pi).data.cpu().numpy()[0]
+        v_np = v.data.cpu().numpy()[0]
+
+        # Combine the action values into a tuple
+        action = pi_np, v_np
+
+        # Use the action tuple to create a move
+        best_move = self.to_move(action)
 
         return best_move
-    
-    def moves_to_tensor(self, board):
-        # Convert the list of legal moves to a tensor
-        moves_tensor = torch.zeros(len(list(board.legal_moves)))
 
-        for i, move in enumerate(list(board.legal_moves)):
-            moves_tensor[i] = move.from_square * 64 + move.to_square
+    def from_move(self, move):
+        return move.from_square*64+move.to_square
 
-        return moves_tensor.unsqueeze(0)  # Add a batch dimension
+    def moves_to_tensor(self, board):        
+        acts = [0] * (64*64)
+        for move in board.legal_moves:
+          acts[self.from_move(move)] = 1
+
+        moves_tensor = torch.FloatTensor(np.array(acts))
+        
+        return moves_tensor
     
+    def to_np(self, board):
+        a = [0] * (8*8*6)
+        for sq, pc in board.piece_map().items():
+            a[sq * 6 + pc.piece_type - 1] = 1 if pc.color else -1
+        return np.array(a)
+        
     def board_to_tensor(self, board):
-        # Convert the chess board state to a tensor
-        # You may want to use one-hot encoding or other representations that your model expects
-        # Here, we use a simple binary representation for the pieces on the board
-        board_tensor = torch.zeros(12, 8, 8)  # 12 channels for 6 piece types and two colors
+        np_board = self.to_np(board)
+        board_tensor = torch.FloatTensor(np_board)
 
-        for square in chess.SQUARES:
-            piece = board.piece_at(square)
-            if piece is not None:
-                piece_index = self.get_piece_index(piece)
-                color_index = 0 if piece.color == chess.WHITE else 1
-                board_tensor[piece_index + color_index * 6][square // 8][square % 8] = 1
+        return board_tensor
 
-        return board_tensor.unsqueeze(0)  # Add a batch dimension
-    
-    def get_piece_index(self, piece):
-        # Map chess piece types to indices (0-5)
-        piece_types = [chess.PAWN, chess.KNIGHT, chess.BISHOP, chess.ROOK, chess.QUEEN, chess.KING]
-        return piece_types.index(piece.piece_type)
 
-    def receive_human_feedback(self, feedback):
-        # You can use feedback from humans to improve your bot's training or fine-tuning
-        # Implement how you want to collect and utilize the feedback
-        pass
 
-    def train(self, training_data):
-        # Train your neural network using the collected feedback and data
-        # You'll need to define your own training pipeline based on your specific project requirements
-        pass
+
+
+    def train(self, examples):
+        """
+            Train on `examples`
+            
+            Args:
+                examples: list of examples, each example is of form 
+                (board, pi, v, valids)
+        """
+        optimizer = optim.Adam(self.nnet.parameters())
+
+        for epoch in range(args.epochs):
+            print('EPOCH ::: ' + str(epoch+1))
+            self.nnet.train()
+            data_time = AverageMeter()
+            batch_time = AverageMeter()
+            pi_losses = AverageMeter()
+            v_losses = AverageMeter()
+            end = time.time()
+
+            batch_idx = 0
+
+            while batch_idx < int(len(examples) / args.batch_size):
+                sample_ids = np.random.randint(len(examples), size=args.batch_size)
+                boards, pis, vs, valids = list(zip(*[examples[i] for i in sample_ids]))
+                boards = torch.FloatTensor(np.array(boards).astype(np.float64))
+                target_pis = torch.FloatTensor(np.array(pis))
+                target_vs = torch.FloatTensor(np.array(vs).astype(np.float64))
+                target_valids = torch.FloatTensor(np.array(valids))
+
+                # Cuda performance improvement
+                boards, target_pis, target_vs, target_valids = boards.contiguous().cuda(), target_pis.contiguous().cuda(), target_vs.contiguous().cuda(), target_valids.contiguous().cuda()
+
+                # measure data loading time
+                data_time.update(time.time() - end)
+
+                # compute output
+                out_pi, out_v = self.nnet((boards, target_valids))
+                l_pi = self.loss_pi(target_pis, out_pi)
+                l_v = self.loss_v(target_vs, out_v)
+                total_loss = l_pi + l_v
+
+                # record loss
+                pi_losses.update(l_pi.item(), boards.size(0))
+                v_losses.update(l_v.item(), boards.size(0))
+                writer.add_scalar("Loss/train", l_pi.item(), batch_idx)
+                writer.add_scalar("Loss/train", l_v.item(), batch_idx)
+                writer.flush()
+                
+                # compute gradient and do SGD step
+                optimizer.zero_grad()
+                total_loss.backward()
+                optimizer.step()
+
+                # measure elapsed time
+                batch_time.update(time.time() - end)
+                end = time.time()
+                batch_idx += 1
+
+                # plot progress
+                log.info('({epoch}: {batch}/{size}) Data: {data:.3f}s | Batch: {bt:.3f}s | Loss_pi: {lpi:.4f} | Loss_v: {lv:.3f} | Total Loss: {tl:.4f}'.format(
+                            batch=batch_idx,
+                            size=int(len(examples)/args.batch_size),
+                            data=data_time.avg,
+                            bt=batch_time.avg,
+                            lpi=pi_losses.avg,
+                            lv=v_losses.avg,
+                            tl=total_loss,
+                            epoch=epoch+1
+                            ))
--- ChessBotModel.py
+++ ChessBotModel.py
@@ -1,20 +1,136 @@
+"""
+    Luna-Chess Reinforcement Neural Network Architecture
+    
+    NOTE
+    Neural Network
+    - Inputs:
+        . b - serialized board
+    - Outputs:
+        . vθ(s) - a scalar value of the board state ∈ [-1,1] from the perspective of the current player
+        . →pθ(s) - a policy that is a probability vector over all possible actions.
+
+    Training
+    (st,→πt,zt), where:
+        - st is the state
+        - →πt is an estimate of the probability from state st
+        - zt final game outcome ∈ [-1,1]
+
+    Loss function:
+    l=∑t(vθ(st)-zt)2-→πt⋅log(→pθ(st))
+"""
+
+from __future__ import annotations
 import torch
-import torch.nn as nn
+from torch import nn
+from torch import optim
+from torch.nn import functional as F
+
+# Luna
+from utils import dotdict
 
 class ChessBotModel(nn.Module):
+    """Reinforcement Learning Neural Network"""
+
+    # Optimizer
+    optimizer: optim.Optimizer
+
+    # Learning Rate
+    learning_rate: float
+
+    # Action size
+    action_size: int
+
+    # HyperParameter args
+    args: dotdict
+
     def __init__(self):
         super(ChessBotModel, self).__init__()
 
-        # Define the layers and architecture of your neural network
-        self.conv1 = nn.Conv2d(12, 64, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(64 * 8 * 8, 256)
-        self.fc2 = nn.Linear(256, 1)  # Output layer with move values
+        self.board_x, self.board_y, self.board_z = (8, 8, 6)
+        self.action_size = 64*64
+        self.args = dotdict({
+            'numIters': 1000,
+            'numEps': 100,                # (100)Number of complete self-play games to simulate during a new iteration.
+            'tempThreshold': 10,        #
+            'updateThreshold': 0.6,     # During arena playoff, new neural net will be accepted if threshold or more of games are won.
+            'maxlenOfQueue': 200000,    # Number of game examples to train the neural networks.
+            'numMCTSSims': 100,         # Number of games moves for MCTS to simulate.
+            'arenaCompare': 20,         # Number of games to play during arena play to determine if new net will be accepted.
+            'cpuct': 1,
+            'checkpoint': './temp/',
+            'load_model': False,
+            'load_examples': True,     # False recommended, so it doesnt overfit net
+            'load_folder_file': ('./pretrained_models/','best.pth.tar'),
+            'numItersForTrainExamplesHistory': 20,
+            'dir_noise': True,
+            'dir_alpha': 1.4,
+            'save_anyway': False,       # Always save model, shouldnt be used
+            'num_channels': 128,
+            'dropout': 0.3,
+        })
 
-    def forward(self, board, legal_moves):
-        x = self.conv1(board)
-        x = self.conv2(x)
-        x = x.view(x.size(0), -1)  # Flatten the tensor
-        x = self.fc1(x)
-        x = self.fc2(x)
-        return x
+        # Define neural net
+        self.define_architecture()
+        self.learning_rate = 1e-3
+        self.optimizer = optim.Adam(self.parameters(), lr=self.learning_rate)
+
+    def define_architecture(self) -> None:
+        """Define Net
+            - Input: serialized chess.Board
+            - Output:
+                - predicted board value (tanh)
+                - policy distribution over possible moves (softmax)
+        """
+        # Args shortcut
+        args = self.args
+
+        # Input
+        self.conv1 = nn.Conv3d(1, args.num_channels, 3, stride=1, padding=1)
+        
+        ## Hidden
+        self.conv2 = nn.Conv3d(args.num_channels, args.num_channels * 2, 3, stride=1, padding=1)
+        self.conv3 = nn.Conv3d(args.num_channels * 2, args.num_channels * 2, 3, stride=1)
+        self.conv4 = nn.Conv3d(args.num_channels * 2, args.num_channels * 2, 3, stride=1)
+        self.conv5 = nn.Conv3d(args.num_channels * 2, args.num_channels, 1, stride=1)
+
+        self.bn1 = nn.BatchNorm3d(args.num_channels)
+        self.bn2 = nn.BatchNorm3d(args.num_channels * 2)
+        self.bn3 = nn.BatchNorm3d(args.num_channels * 2)
+        self.bn4 = nn.BatchNorm3d(args.num_channels * 2)
+        self.bn5 = nn.BatchNorm3d(args.num_channels)
+
+        self.fc1 = nn.Linear(args.num_channels*(self.board_x-4)*(self.board_y-4)*(self.board_z-4), 1024) #4096 -> 1024
+        self.fc_bn1 = nn.BatchNorm1d(1024)
+
+        self.fc2 = nn.Linear(1024, 512)
+        self.fc_bn2 = nn.BatchNorm1d(512)
+
+        self.fc3 = nn.Linear(512, 512)
+        self.fc_bn3 = nn.BatchNorm1d(512)
+
+        # output p dist        
+        self.fc4 = nn.Linear(512, self.action_size)
+
+        # output scalar
+        self.fc5 = nn.Linear(512, 1)
+
+    def forward(self, boardsAndValids):
+        """Forward prop"""
+        x, valids = boardsAndValids
+
+        x = x.view(-1, 1, self.board_x, self.board_y, self.board_z)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = F.relu(self.bn5(self.conv5(x)))
+        x = x.view(-1, self.args.num_channels*(self.board_x-4)*(self.board_y-4)*(self.board_z-4))
+        x = F.dropout(F.relu(self.fc_bn1(self.fc1(x))), p=self.args.dropout, training=self.training)
+        x = F.dropout(F.relu(self.fc_bn2(self.fc2(x))), p=self.args.dropout, training=self.training)
+        x = F.dropout(F.relu(self.fc_bn3(self.fc3(x))), p=self.args.dropout, training=self.training)
+
+        pi = self.fc4(x)
+        v = self.fc5(x)
+
+        pi -= (1 - valids) * 1000
+        return F.log_softmax(pi, dim=1), torch.tanh(v)
